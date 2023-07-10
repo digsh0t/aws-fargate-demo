6e657bcd46fc41f3c761d8adf1e5c43f3e677ce7
digsh0t <wintltr@gmail.com>
2023-07-10 22:16:11 +0700

    add module vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
   region = "us-east-2"
+
+  default_tags {
+    tags = {
+      Stack       = var.name
+      Environment = var.environment
+    }
+  }
 }
 
 terraform {
@@ -7,3 +14,11 @@ terraform {
     key = "aws-fargate-demo-bucket/terraform/main.tf"
   }
 }
+
+# module "vpc" {
+#   source             = "./modules/global/vpc"
+#   cidr               = var.cidr
+#   availability_zones = var.availability_zones
+#   private_subnets    = var.private_subnets
+#   public_subnets     = var.public_subnets
+# }
